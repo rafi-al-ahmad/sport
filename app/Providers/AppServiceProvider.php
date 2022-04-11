da3a18f80dfd382403fcd87dca4d1765b42f9cd9
rafi <rafi@mail.com>
2022-04-11 16:26:25 +0300

    telescope integration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
+use Intervention\Image\ImageManagerStatic;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment('local')) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
@@ -23,6 +28,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('imageable', function ($attribute, $value, $params, $validator) {
+            try {
+                ImageManagerStatic::make($value);
+                return true;
+            } catch (\Exception $e) {
+                return false;
+            }
+        });
     }
 }
